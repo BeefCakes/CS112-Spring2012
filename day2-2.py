9404c624110ddaca1abd8e2cfe6d5588841fc4cf
Jordan Miron <CalamariDamacy@gmail.com>
2012-01-31 16:46:46 -0500

    hw02 done

---
+++ day2-2.py
@@ -0,0 +1,9 @@
+"""
+day2-2.py 
+
+A first program by Jordan Miron
+"""
+print "hello nurse"
+print "1 + 3=" ,1+3 # , allows you to join multiple things
+print "2 * 8=" ,2*8
+print "that was uber exciting"
